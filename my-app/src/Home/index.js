6e2a6dd2e8cbf8201effff668e7b31e68e498824
ortiz-da <ortiz.da@northeastern.edu>
2023-04-11 13:06:42 -0400

    middle of working on latest user display on homescreen

--- my-app/src/Home/index.js
+++ my-app/src/Home/index.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Navigation from "../Navigation";
 import GameSummaryComponent from "../GameComponents/game-summary-component";
+import {findAllUsersThunk, profileThunk} from "../Thunks/user-thunks";
+import {useDispatch} from "react-redux";
 
 const HomePage = () => {
+    const dispatch = useDispatch();
 
+
+    const [users, setUsers] = useState([])
+
+    const getUsers = async () => {
+        // const profile = await userService.profile();
+        const action = await dispatch(findAllUsersThunk());
+        setUsers(action.payload);
+    };
+
+    useEffect(() => {
+        // if (username) {
+        //     getUserByUsername();
+        // } else {
+        //     getProfile();
+        // }
+        getUsers();
+
+    }, []);
 
 
     return(
@@ -13,6 +34,9 @@ const HomePage = () => {
 
             <h2>Latest Post</h2>
             <h2>Newest User</h2>
+
+            {/*// https://stackoverflow.com/questions/37542093/get-the-last-element-in-json-array*/}
+            {/*{JSON.stringify(users[users.length-1].username)}*/}
         </div>
     )
 }
