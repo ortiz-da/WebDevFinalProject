b18aa1663172831b16faa2da75b963a16bf8a88e
ortiz-da <ortiz.da@northeastern.edu>
2023-04-20 13:50:28 -0400

    comments-service.js fixed

--- my-app/src/Services/comments-service.js
+++ my-app/src/Services/comments-service.js
@@ -3,23 +3,27 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const COMMENTS_API = `${API_BASE}/comments`;
 
+const api = axios.create({
+    withCredentials: true,
+});
+
 export const createComment = async (comment) => {
-    const response = await axios.post(COMMENTS_API, comment)
+    const response = await api.post(COMMENTS_API, comment)
     return response.data;
 }
 export const findComments  = async ()     => {
-    const response = await axios.get(COMMENTS_API);
+    const response = await api.get(COMMENTS_API);
     const comments = response.data;
     return comments;
 }
 export const deleteComment = async (cid) => {
-    const response = await axios
+    const response = await api
         .delete(`${COMMENTS_API}/${cid}`)
     return response.data
 }
 export const updateComment = async (comment) => {
 
-    const response = await axios
+    const response = await api
         .put(`${COMMENTS_API}/${comment._id}`, comment);
 
     return comment;
